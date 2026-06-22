336d4f9a7ce6e596601132c0cb63135cda5a4a19
ChaeHyunLim <chaehyun010104@gmail.com>
2026-06-22 12:27:10 +0900

    setting: test 관련 설정 변경

--- src/test/java/com/mju/capstone_backend/CapstoneBackendApplicationTests.java
+++ src/test/java/com/mju/capstone_backend/CapstoneBackendApplicationTests.java
@@ -1,17 +1,9 @@
 package com.mju.capstone_backend;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
 
-import java.util.UUID;
-
-@SpringBootTest
+@Suite
+@SelectPackages("com.mju.capstone_backend")
 class CapstoneBackendApplicationTests {
-
 }
