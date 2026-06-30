3a7c4585300fdc9c936a0ae5294d826d408cddd1
ChaeHyunLim <chaehyun010104@gmail.com>
2026-06-30 12:26:04 +0900

    fix(#5): user 회원가입 중복 요청 방지 로직 추가

--- src/main/java/com/mju/capstone_backend/domain/user/repository/UserRepository.java
+++ src/main/java/com/mju/capstone_backend/domain/user/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package com.mju.capstone_backend.domain.user.repository;
 
 import com.mju.capstone_backend.domain.user.entity.User;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveCrudRepository<User, String> {
+
+    @Query("INSERT INTO users (clerk_id) VALUES (:clerkId) ON CONFLICT (clerk_id) DO NOTHING")
+    Mono<Void> insertIfNotExists(String clerkId);
 }
